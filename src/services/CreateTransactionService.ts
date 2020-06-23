241bbebb8467347058f40d2afae8da94146c3a84
Yuri Weilemann <yuri_weilemann@hotmail.com>
2020-06-23 10:28:40 -0300

    Challenge #5 completed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('This type of transaction is not recognized.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type == 'outcome' && total < value) {
+      throw Error(
+        'The outcome is higher than the total amount in your account',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
